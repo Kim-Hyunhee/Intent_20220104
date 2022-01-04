ca2e706643acf6b1a471a55e1b706c039eb5d575
김현희 <tta_bi@naver.com>
2022-01-04 10:35:03 +0900

    화면 이동 시 데이터 첨부 (putExtra)

--- app/src/main/java/com/nepplus/intent_20220104/MainActivity.kt
+++ app/src/main/java/com/nepplus/intent_20220104/MainActivity.kt
@@ -13,10 +13,15 @@ class MainActivity : AppCompatActivity() {
         btnSendMessage.setOnClickListener {
 
             // 입력한 내용을 추출
-            val inpitMessage = edtMessage.text.toString()
+            val inputMessage = edtMessage.text.toString()
 
             // 메기지 보기 화면으로 이동
             val myIntent = Intent (this, ViewMessageActivity::class.java)
+
+            // myIntent에 추가 정보 담기 (여러 개의 데이터 추가도 가능)
+            myIntent.putExtra("message",inputMessage)
+            // myIntent.pueExtra()
+
             startActivity(myIntent)
 
         }
